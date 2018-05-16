780016794c74afc604d1eb65f2bb573bdca3cf98
Eli Most <mosteli@grinnell.edu>
2018-05-16 11:24:36 -0500

    Instructions change based on which device is being used.

---
+++ Assets/Scripts/GCIEL_Toolkit/SO_Variables/StringVariable.cs
@@ -0,0 +1,35 @@
+﻿// ----------------------------------------------------------------------------
+// GCIEL - Scriptable Object Variables
+// 
+// Author: Zachary Segall
+// Date:   02/13/2018
+//
+// Modeled after Game Architecture with Scriptable Objects 
+// Author: Ryan Hipple
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+namespace GCIEL.Toolkit
+{
+    [CreateAssetMenu]
+    public class StringVariable : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [Multiline]
+        public string DeveloperDescription = "";
+#endif
+        [TextArea]
+        public string Value;
+
+        public void SetValue(string value)
+        {
+            Value = value;
+        }
+
+        public void SetValue(StringVariable value)
+        {
+            Value = value.Value;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/UI/DeviceToggle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GCIEL.Toolkit;
+using UnityEngine.XR;
+
+/***
+ * Toggles between text based on which device the user is using
+ * TODO: Generalize code as necessary
+ ***/
+public class DeviceToggle : MonoBehaviour
+{
+
+    private Text textbox;
+    public StringReference RiftText;
+    public StringReference ViveText;
+
+    // Use this for initialization
+    void Start()
+    {
+        textbox = GetComponent<Text>();
+        if(XRDevice.isPresent)
+        {
+            if(XRDevice.model.Contains("Rift"))
+            {
+                textbox.text = RiftText.Value;
+            }
+            else
+            {
+                textbox.text = ViveText.Value;
+            }
+        }
+    }
+    
+}
